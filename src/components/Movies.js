a4dd58e1c101e3c6a402ab783e95d5630935f253
DevinPierce <devinmpierce@gmail.com>
2018-08-14 10:12:06 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+          <div>
+            <h2>{movie.title}</h2>
+            <p>Time: {movie.time}</p>
+            {movie.metascore ? <p>Metascore: {movie.metascore}</p> : null}
+            <h3>Genres:</h3>
+            <ul>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
